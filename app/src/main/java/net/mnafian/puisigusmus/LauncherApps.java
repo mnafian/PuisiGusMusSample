21ce05cf2416fce139a19aae5f3d8f12a4ac3db9
M. Nafian <mnafian@icloud.com>
2015-08-31 13:26:34 +0700

    Change launcher style and cardview margin.

--- app/src/main/java/net/mnafian/puisigusmus/LauncherApps.java
+++ app/src/main/java/net/mnafian/puisigusmus/LauncherApps.java
@@ -2,10 +2,12 @@ package net.mnafian.puisigusmus;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 /**
  * Created by mnafian on 8/29/15.
@@ -19,6 +21,13 @@ public class LauncherApps extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.puisi_splashscreen);
+
+        Typeface blockFonts = Typeface.createFromAsset(getAssets(),"CarterOne.ttf");
+        TextView txtSampleTxt = (TextView) findViewById(R.id.gus_mustx);
+        txtSampleTxt.setTypeface(blockFonts);
+
+        TextView txtSampleTxt1 = (TextView) findViewById(R.id.gus_mustx_1);
+        txtSampleTxt1.setTypeface(blockFonts);
 
         new Handler().postDelayed(new Runnable() {
             @Override
